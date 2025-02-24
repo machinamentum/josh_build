aa54811e10b75af1c69a4c478cdf6ad5768d8007
Joshua Huelsman <joshuahuelsman@gmail.com>
2025-02-24 12:37:38 -0500

    Use jb_generate_embed in embed.c to deduplicate work.

--- src/embed.c
+++ src/embed.c
@@ -1,46 +1,6 @@
 
-#include <stdio.h>
-#include <stdlib.h>
-
-char *read_file(const char *path) {
-	FILE *f = fopen(path, "rb");
-
-	if (!f)
-		return NULL;
-
-	fseek(f, 0, SEEK_END);
-	long len = ftell(f);
-	fseek(f, 0, SEEK_SET);
-
-	char *out = malloc(len + 1);
-
-	size_t read = fread(out, 1, len, f);
-
-	if (read != len)
-		return NULL;
-
-	out[len] = 0;
-
-	return out;
-}
-
-void generate_embed(const char *input, const char *output) {
-	char *text = read_file(input);
-
-	FILE *out = fopen(output, "wb");
-
-	while (*text) {
-		fprintf(out, "0x%X", *text);
-		text++;
-
-		if (*text != 0)
-			fprintf(out, ", ");
-	}
-
-	fputc('\n', out);
-
-	fclose(out);
-}
+#define JOSH_BUILD_IMPL
+#include "josh_build.h"
 
 int main(int argc, char **argv) {
 	if (argc < 3) {
@@ -48,6 +8,6 @@ int main(int argc, char **argv) {
 		return 1;
 	}
 
-	generate_embed(argv[1], argv[2]);
+	jb_generate_embed(argv[1], argv[2]);
 	return 0;
 }
